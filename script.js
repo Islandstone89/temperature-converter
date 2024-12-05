2cd2232570a54735e0e6ac63ca01bb90fe933b05
Øystein Håberg <islandstone89@gmail.com>
2024-12-05 23:55:25 +0100

    * Added js functionality

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let textInput = document.getElementById("tempInput");
+let radioBtn1 = document.getElementById("tempScale1");
+let radioBtn2 = document.getElementById("tempScale2");
+let convertBtn = document.getElementById("convertBtn");
+let result = document.getElementById("conversionResult");
+let errorMessage = document.getElementById("errorMessage");
+
+let missingValue = "Please enter a value";
+let missingNumber = "Please enter a number";
+let emptyString = ""
+let celcius = "C";
+let fahrenheit = "F";
+
+
+function convertTemp() {
+    temp = textInput.value;
+
+    if (temp === "") {
+        errorMessage.textContent = missingValue;
+    }
+
+    else if (isNaN(temp)) {
+        errorMessage.textContent = missingNumber;
+    }
+
+    else if (radioBtn1.checked) {
+        errorMessage.textContent = emptyString;
+        let convertedTemp = (temp - 32) * 5 / 9;
+        result.textContent = `${convertedTemp.toFixed(1)} °${celcius}`;
+    }
+
+    else if (radioBtn2.checked) {
+        errorMessage.textContent = emptyString;
+        let convertedTemp = temp * 9 / 5 + 32;
+        result.textContent = `${convertedTemp.toFixed(1)} °${fahrenheit}`;
+    }
+}
+
+convertBtn.addEventListener("click", convertTemp);
